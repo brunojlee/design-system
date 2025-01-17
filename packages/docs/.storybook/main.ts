8ec0d1a906d17848143c999a65592d6c19de4089
Bruno Roja <brunojlee@hotmail.com>
2025-01-17 16:27:36 -0400

    feat: Adicionando build configs
    Signed-off-by: Bruno Roja <brunojlee@hotmail.com>

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,40 +1,34 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-import { join, dirname } from "path";
+import type { StorybookConfig } from '@storybook/react-vite';
+import { dirname, join } from 'path';
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 
 const config: StorybookConfig = {
   stories: [
-    "../src/stories/**/*.stories.tsx", // Update path to match your stories location
-    "../src/stories/pages/**/*.stories.tsx"    // Add MDX stories path
+    '../src/stories/**/*.stories.tsx', // Update path to match your stories location
+    '../src/stories/pages/**/*.stories.tsx', // Add MDX stories path
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-docs", // Add docs addon explicitly
-    "@storybook/addon-a11y"
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: true,
-  },
-  viteFinal: (config)=>{
-
+  viteFinal: (config) => {
     config.build = config.build || {};
     config.build.outDir = 'storybook-static';
+    config.base = '/design-system/'; // Adicione esta linha
 
-    if(process.env.STORYBOOK_DEPLOY === 'true'){
-      config.base = '/design-system/'
+    if (process.env.STORYBOOK_DEPLOY === 'true') {
+      config.base = '/design-system/';
     }
     return config;
-  }
+  },
 };
-
 
 export default config;
--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/design-system/',
   plugins: [react()],
-})
+  // ... outras configs
+});
